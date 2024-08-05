a550699c57bfafa42af041185f6154029bfc99b8
David Mphande <102615835+Heiteinany18@users.noreply.github.com>
2024-08-05 11:56:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let ans = document.querySelector("p");
+let correctAnswer = "Irritability"; 
+let inputed = document.querySelector(".answer");
+
+//ans.innerHTML = "Hello";
+       
+function clicky() {
+
+   if (inputed == "Irritability") {
+       ans.innerText = "Correct";
+   }
+   else {
+       ans.innerHTML = "Incorrect\n\n";
+       ans.innerHTML = "persecuti aliquip epicuri posuere honestatis comprehensam tale inciderint amet id habemus tristique antiopam audire tellus maecenas inani omittam pertinax sonet"
+   }
+}
+        
